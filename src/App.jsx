8929080da9d595e7cd7dc0a7edbe6a4888d0d874
Yaroslaw <xxjokerxx933@gmail.com>
2024-06-06 15:20:10 +0300

    add search query, check query

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import getImages from './images-api';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+
+  const onSubmit = async (query) => {
+    try {
+      const images = await getImages(query);
+      setImages(images);
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   return (
     <>
-      <div>Hello</div>
+      <div>
+        <SearchBar onSubmit={onSubmit} />
+        {error && <p>Помилка: {error}</p>}
+        <div>
+          {images.map((image) => (
+            <img key={image.id} src={image.urls.small} alt={image.description} />
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/images-api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+
+const UNSPLASH_ACCESS_KEY = 'E1cdy3dv_KADegi36FDRDy7wAcwRFX8WkgPlx-8266g';  // Замініть на свій ключ доступу
+
+async function getImages(query, perPage = 10, page = 1) {
+    const url = `https://api.unsplash.com/search/photos`;
+
+    try {
+        const response = await axios.get(url, {
+            params: {
+                query: query,
+                per_page: perPage,
+                page: page
+            },
+            headers: {
+                Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
+            }
+        });
+
+        return response.data.results;
+    } catch (error) {
+        console.error('Помилка при отриманні зображень з Unsplash:', error.message);
+        console.error('Деталі помилки:', error.response?.data || error);
+        throw error;
+    }
+}
+
+async function fetchImages() {
+    try {
+        const images = await getImages('nature');
+        console.log('Отримані зображення:', images);
+    } catch (error) {
+        console.error('Помилка:', error.message);
+    }
+}
+
+fetchImages();
+
+
+export default getImages;
